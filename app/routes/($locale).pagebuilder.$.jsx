878c3e016e7fd144a61cb39a8559cac546f9a8d7
William Tetlow <william@getshogun.com>
2023-09-21 10:41:08 +0100

    feat: add stale while revalidate cache header

--- app/routes/($locale).pagebuilder.$.jsx
+++ app/routes/($locale).pagebuilder.$.jsx
@@ -5,6 +5,15 @@ import {PageBuilderPage, getPageBuilderPage} from '~/pagebuilder';
 const BASE_PATH = '/pagebuilder';
 
 /*
+ * We cache the page for 30s and set stale-while-revalidate to 30s
+ *
+ * This means at worst case users will be served a stale page for 1 minute after a new version becomes available
+ *
+ * You can change these values https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#stale-while-revalidate
+ */
+const CACHE_CONTROL = 'public, max-age=30, stale-while-revalidate=30';
+
+/*
  * Request the page with path from Shogun PageBuilder
  *
  * e.g. /pagebuilder/my-blog -> requests /my-blog from Shogun PageBuilder
@@ -18,7 +27,10 @@ export async function loader({request, context}) {
   const pbPage = await getPageBuilderPage(storeDomain, path);
 
   return new Response(pbPage, {
-    headers: {'Content-Type': 'application/text'},
+    headers: {
+      'Content-Type': 'application/text',
+      'Cache-Control': CACHE_CONTROL,
+    },
   });
 }
 
